4112c8a4db020f93a36a2ef04636f7f6712125c6
XionngDa1024 <1552387702@qq.com>
2021-10-06 19:39:51 +0800

    提交2

--- TestDemo/TestDemo/ViewController.m
+++ TestDemo/TestDemo/ViewController.m
@@ -17,6 +17,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor redColor];
+
 }
 
 
